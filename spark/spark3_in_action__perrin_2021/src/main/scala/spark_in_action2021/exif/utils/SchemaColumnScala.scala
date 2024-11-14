01ceecefaf7996e12e52bcc63c390f8b1b80f47e
vpostrigan <postriganvova@gmail.com>
2024-11-14 23:14:19 +0200

    refactoring

--- spark/spark3_in_action__perrin_2021/src/main/scala/spark_in_action2021/exif/utils/SchemaColumnScala.scala
+++
@@ -1,8 +0,0 @@
-package spark_in_action2021.exif.utils
-
-/**
- * To represent Schema column details
- *
- * @author rambabu.posa
- */
-case class SchemaColumnScala(methodName: String, columnName: String)
